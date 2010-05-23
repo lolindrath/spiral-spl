d75d068bede937ba28accb0c8b6e5b61ba70421e
Andy Williams <lolindrath@gmail.com>
2010-05-22 21:14:13 -0400

    wrote a run script to do an end to end compile

---
+++ run.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#
+#
+
+mkdir -p tmp
+pushd tmp
+rm -f *.spl *.icode *.cpp *.exe
+
+cp ../test/templates.spl templates.spl
+
+../program_gen/program_gen $1
+if [ "$?" -ne "0" ] ; then
+	echo "Program gen failed!"
+	exit 1
+fi
+
+for file in ./fft*.spl ; do
+	cat $file | ../spl2icode/spl $1 1> ${file}.icode
+	if [ "$?" -ne "0" ] ; then
+		echo "ICODE gen failed!"
+		exit 1
+	fi
+
+	cat ${file}.icode | ../icode2cpp/icode2c $1 1> ${file}.icode.cpp
+	if [ "$?" -ne "0" ] ; then
+		echo "CPP gen failed!"
+		exit 1
+	fi
+
+	g++ -o $file.exe -O3 $file.icode.cpp
+	if [ "$?" -ne "0" ] ; then
+		echo "COMPILE failed!"
+		exit 1
+	fi
+done
+
+popd
